f0ef07aa370d9fc7ce6b1bae5000c386fa58f895
iFaxity <izze96@gmail.com>
2019-01-27 21:14:49 +0100

    Fix: child_process & readline

--- child_process.js
+++ child_process.js
@@ -3,5 +3,5 @@ const child_process = require('child_process');
 
 module.exports = promisifyAll(child_process, [
   'exec',
-  'execFile'
+  'execFile',
 ]);
--- readline.js
+++ readline.js
@@ -1,10 +1,33 @@
 const readline = require('readline');
 
+function promisifyCompleter(completer) {
+  return (line, callback) => {
+    const res = completer(line);
+
+    // Check if res is a thenable
+    if (typeof res.then != 'function') {
+      return callback(null, res);
+    }
+
+    res.catch(callback).then(_ => callback(null, _));
+  }
+}
+
 class AsyncInterface extends readline.Interface {
   constructor(stdin, stdout, completer, term) {
     if (arguments.length == 1) {
-      super(stdin);
+      const opts = Object.assign({}, stdin);
+
+      if (typeof stdin.completer == 'function') {
+        opts.completer = promisifyCompleter(stdin.completer);
+      }
+
+      super(opts);
     } else {
+      if (typeof completer == 'function') {
+        completer = promisifyCompleter(completer);
+      }
+
       super(stdin, stdout, completer, term);
     }
   }
